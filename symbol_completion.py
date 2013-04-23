f21790cb5603230318a91d7d08fce8c89743443a
jagt <jagt@live.com>
2013-04-23 23:07:01 +0800

    initial commit

---
+++ symbol_completion.py
@@ -0,0 +1,22 @@
+import sublime, sublime_plugin
+import re
+
+word_pattern = re.compile('\w+')
+
+class SymbolCompletion(sublime_plugin.EventListener):
+	def on_query_completions(self, view, prefix, locations):
+		completions = []
+		# move current view to the start
+		views = sublime.active_window().views()
+		views.remove(view)
+		views.insert(0, view)
+
+		keys = set()
+		for v in views:
+			for symbol in v.symbols():
+				key = word_pattern.search(symbol[1]).group()
+				if not key in keys:
+					keys.add(key)
+					completions.append((key, view.file_name()))
+
+		return completions
