0de6293f7e66dc0fc54647b0b2f4dc3977045f8c
jagt <jagt@live.com>
2013-04-23 23:28:45 +0800

    tabs to spaces

--- symbol_completion.py
+++ symbol_completion.py
@@ -4,19 +4,19 @@ import re
 word_pattern = re.compile('\w+')
 
 class SymbolCompletion(sublime_plugin.EventListener):
-	def on_query_completions(self, view, prefix, locations):
-		completions = []
-		# move current view to the start
-		views = sublime.active_window().views()
-		views.remove(view)
-		views.insert(0, view)
+    def on_query_completions(self, view, prefix, locations):
+        completions = []
+        # move current view to the start
+        views = sublime.active_window().views()
+        views.remove(view)
+        views.insert(0, view)
 
-		keys = set()
-		for v in views:
-			for symbol in v.symbols():
-				key = word_pattern.search(symbol[1]).group()
-				if not key in keys:
-					keys.add(key)
-					completions.append((key, view.file_name()))
+        keys = set()
+        for v in views:
+            for symbol in v.symbols():
+                key = word_pattern.search(symbol[1]).group()
+                if not key in keys:
+                    keys.add(key)
+                    completions.append((key, key))
 
-		return completions
+        return completions
